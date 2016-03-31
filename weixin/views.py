584ec96e3521ac650422be5092f44453a8875d48
stephen <youqiukun@163.com>
2016-03-31 15:38:17 +0800

    modify weixin/views.py

--- weixin/views.py
+++ weixin/views.py
@@ -22,70 +22,65 @@ conf = WechatConf(
 
 wechat = WechatBasic(conf = conf)
 
-REDIRECT_URL = 'http://ec2-54-200-11-160.us-west-2.compute.amazonaws.com/weixin/snd_bonus'
-OAUTH_URL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URL&response_type=code&scope=snsapi_base&state=1#wechat_redirect"
-
 # Create your views here.
 
 #test asp
 @csrf_exempt
 def asp_test(request):
-	#name = request.POST.get('name')
-	#city = request.POST.get('city')
 	print('recive asp_test request\n')
 	response = HttpResponse()
-        response['Access-Control-Allow-Origin'] = 'http://www.w3school.com.cn'
-        response.write('request ok')
-        return response
+	response['Access-Control-Allow-Origin'] = '*'
+	response.write('request ok')
+	return response
 
 #发红包	
 def snd_bonus(request):
-	print("snd_bonus:%s\n" %(request))
 	temp = get_template('fahongbao.html')
 	html = temp.render({'STATIC_URL': settings.STATIC_URL},request)
-	return HttpResponse(html)	
+	eturn HttpResponse(html)	
 
 #抢红包
 @csrf_exempt
 def rcv_bonus(request):
-    temp = get_template('qianghongbao.html')
-    html = temp.render({'STATIC_URL': settings.STATIC_URL},request)
-    return HttpResponse(html)
+	temp = get_template('qianghongbao.html')
+	html = temp.render({'STATIC_URL': settings.STATIC_URL},request)
+	return HttpResponse(html)
 
 #抢到的红包
 @csrf_exempt
 def geted_bonus(request):
-    temp = get_template('qiangdaohongbao.html')
-    html = temp.render({'STATIC_URL': settings.STATIC_URL},request)
-    return HttpResponse(html)
+	temp = get_template('qiangdaohongbao.html')
+	html = temp.render({'STATIC_URL': settings.STATIC_URL},request)
+	return HttpResponse(html)
     
 
 @csrf_exempt
 def token(request):
-    if request.method == 'GET':
-        # 检验合法性
-        # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
-        signature = request.GET.get('signature')
-        timestamp = request.GET.get('timestamp')
-        nonce = request.GET.get('nonce')
+	if request.method == 'GET':
+		# 检验合法性
+		# 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
+		signature = request.GET.get('signature')
+		timestamp = request.GET.get('timestamp')
+		nonce = request.GET.get('nonce')
 
-        if not wechat.check_signature(
-                signature, timestamp, nonce):
-            return HttpResponseBadRequest('Verify Failed')
+		if not wechat.check_signature(
+				signature, timestamp, nonce):
+			return HttpResponseBadRequest('Verify Failed')
 
-        return HttpResponse(
-            request.GET.get('echostr', ''), content_type="text/plain")  
+		return HttpResponse(
+			request.GET.get('echostr', ''), content_type="text/plain")  
 
-    try:
-        wechat.parse_data(data = request.body)
-    except ParseError:
-        return HttpResponseBadRequest('Invalid XML Data')
-    print(request.body)
-    print('\n********\n')
-    response = wechat.response_text(content ='')
-    if isinstance(wechat.message, messages.EventMessage):
-        if wechat.message.type == 'subscribe':
-            response = wechat.response_text(content = u'您已入座%s号桌' %(re.findall(r'\d+',wechat.message.key)[0]))
-        elif wechat.message.type == 'scan':
-            response = wechat.response_text(content =  u'您已入座%s号桌' %(wechat.message.key))
-    return HttpResponse(response, content_type='application/xml')
+	try:
+		wechat.parse_data(data = request.body)
+	except ParseError:
+		return HttpResponseBadRequest('Invalid XML Data')
+		
+	print("****POST request body:%s\n" %(request.body))
+	
+	response = wechat.response_text(content ='')
+	if isinstance(wechat.message, messages.EventMessage):
+		if wechat.message.type == 'subscribe':
+			response = wechat.response_text(content = u'您已入座%s号桌' %(re.findall(r'\d+',wechat.message.key)[0]))
+		elif wechat.message.type == 'scan':
+			response = wechat.response_text(content =  u'您已入座%s号桌' %(wechat.message.key))
+	return HttpResponse(response, content_type='application/xml')
