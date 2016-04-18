cdfdf790ab1f2a09b1f3a3ff3d9bbf20eae115b2
stephen <youqiukun@163.com>
2016-04-18 14:27:28 +0800

    创建初始化数据记录

--- weixin/admin.py
+++ weixin/admin.py
@@ -39,64 +39,37 @@ null_system_recharge = 1
 null_ticket = 1
 null_rcv_bonus = 1
 
-'''
-table = DiningTable.objects.all()
-table.delete()
-table = DiningTable.objects.create(index_table='0')
-table.save()
+
+table = DiningTable.objects.get_or_create(index_table='0')
+null_dining_table=table[0]
 
 
-consumer = Consumer.objects.all()
-consumer.delete()
-consumer = Consumer.objects.create(open_id='2000000000', name='null', on_table=table)
-consumer.save()
+consumer = Consumer.objects.get_or_create(open_id='2000000000', name='null', on_table=null_dining_table)
+null_consumer=consumer[0]
 
 
-person_recharge = PersonRecharge.objects.all()
-person_recharge.delete()
-person_recharge = PersonRecharge.objects.create(id_recharge=2000000000, recharge_person=consumer)
-person_recharge.save()
 
-system_recharge = SystemRecharge.objects.all()
-system_recharge.delete()
-system_recharge = SystemRecharge.objects.create(id_recharge=2000000000)
-system_recharge.save()
-
-ticket = Ticket.objects.all()
-ticket.delete()
-ticket = Ticket.objects.create(id_ticket=2000000000, consumer=consumer)
-ticket.save()
-
-person_bonus = PersonBonus.objects.all()
-person_bonus.delete()
-person_bonus = PersonBonus.objects.create(id_bonus=2000000000, consumer=consumer)
-person_bonus.save()
-
-#system_bonus = SystemBonus.objects.all()
-#system_bonus.delete()
-'''
-
-'''
-system_bonus = SystemBonus.objects.all()
-system_bonus.delete()
-system_bonus = SystemBonus.objects.create(id_bonus=2000000000)
-system_bonus.save()
-
-rcv_bonus = RcvBonus.objects.all()
-rcv_bonus.delete()
-rcv_bonus = RcvBonus.objects.create(id_bonus=2000000000, person_bonus=person_bonus, system_bonus=system_bonus, consumer=consumer, table=table)
-rcv_bonus.save()
-'''
+person_recharge = PersonRecharge.objects.get_or_create(id_recharge=2000000000, recharge_person=null_consumer)
+null_person_recharge=person_recharge[0]
 
 
-'''
-null_dining_table = DiningTable.objects.get(index_table='0')
-null_consumer = Consumer.objects.get(open_id='2000000000')
-null_person_recharge = PersonRecharge.objects.get(id_recharge=2000000000)
-null_system_recharge = SystemRecharge.objects.get(id_recharge=2000000000)
-null_ticket = Ticket.objects.get(id_ticket=2000000000)
-null_person_bonus = PersonBonus.objects.get(id_bonus=2000000000)
-null_system_bonus = SystemBonus.objects.get(id_bonus=2000000000)
-null_rcv_bonus = RcvBonus.objects.get(id_bonus=2000000000)		
-'''
+system_recharge = SystemRecharge.objects.get_or_create(id_recharge=2000000000)
+null_system_recharge=system_recharge[0]
+
+
+ticket = Ticket.objects.get_or_create(id_ticket=2000000000, consumer=null_consumer)
+null_ticket=ticket[0]
+
+
+person_bonus = PersonBonus.objects.get_or_create(id_bonus=2000000000, consumer=null_consumer)
+null_person_bonus=person_bonus[0]
+
+
+
+system_bonus = SystemBonus.objects.get_or_create(id_bonus=2000000000)
+null_system_bonus=system_bonus[0]
+
+
+rcv_bonus = RcvBonus.objects.get_or_create(id_bonus=2000000000, person_bonus=null_person_bonus, system_bonus=null_system_bonus, consumer=null_consumer, table=null_dining_table)
+null_rcv_bonus=rcv_bonus[0]
 
