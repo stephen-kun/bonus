8805554edaba6ad93127bcc362d3971fb52c6b9f
Hero <511656717@qq.com>
2016-04-21 09:25:25 +0800

    后台管理界面完成用户管理功能

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'^login/$', auth_views.login, {'template_name':'manager/login.html'}, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 
     url(r'^bonus_info/$', views.bonus_info, name='bonus_info'),
     url(r'^bonus_info/send_bonus/$', views.send_bonus_statistic, name="send_bonus_statistic"),
@@ -15,6 +19,7 @@ urlpatterns = [
     url(r'^account/$', views.account, name='account'),
     url(r'^account/manage/$', views.account_manage, name='account_manage'),
     url(r'^account/create/$', views.account_create, name='create_account'),
+    url(r'^account/action/?$', views.action, name='action'),
     url(r'^account/change_password/$', views.change_password, name='change_password'),
     url(r'^account/create_bonus/$', views.create_bonus, name='create_bonus'),
     url(r'^account/set_bonus_limit/$', views.set_bonus_limit, name='set_bonus_limit'),
