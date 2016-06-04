e103f86352510a5ef43ff00333b5882b27031c44
stephen <youqiukun@163.com>
2016-06-04 15:49:13 +0800

    modify test url

--- bonus/settings.py
+++ bonus/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'bonus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qubaba',
+        'NAME': 'koovox',
 			'USER': 'root',
 			'PASSWORD': 'youqiukun',
 			'HOST':'',
