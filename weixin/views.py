3a5a81c9d087123ae5b6c2d04a84d1cd2304009a
stephen <youqiukun@163.com>
2016-06-22 07:31:17 +0800

    filter remove topic

--- weixin/views.py
+++ weixin/views.py
@@ -66,11 +66,11 @@ def get_slideimages(request):
 @csrf_exempt
 def get_forums(request):
 	data = {}
-	topics_len = Topic.objects.order_by('-is_globally_pinned', '-is_pinned', '-last_active').count()
+	topics_len = Topic.objects.filter(is_removed=False).order_by('-is_globally_pinned', '-is_pinned', '-last_active').count()
 	if topics_len > 10:
-		topic_list = Topic.objects.order_by('-is_globally_pinned', '-is_pinned', '-last_active')[0:10]
+		topic_list = Topic.objects.filter(is_removed=False).order_by('-is_globally_pinned', '-is_pinned', '-last_active')[0:10]
 	else:
-		topic_list = Topic.objects.order_by('-is_globally_pinned', '-is_pinned', '-last_active')
+		topic_list = Topic.objects.filter(is_removed=False).order_by('-is_globally_pinned', '-is_pinned', '-last_active')
 	topics = []
 	for topic in topic_list:
 		comments = Comment.objects.for_topic(topic=topic).order_by('date')
@@ -257,18 +257,15 @@ def check_consumer_code(request):
 		log_print(check_consumer_code)
 		return _response_json(3, "错误")
 
-#请桌接口
+#清桌接口
 @csrf_exempt
 def release_dining_table(request):		
 	try:
 		index_table = str(request.POST.get('table'))
 		table = DiningTable.objects.get(index_table=index_table)
 		if table.status:
-			consumer_list = Consumer.objects.filter(on_table=table)
-			for consumer in consumer_list:
-				consumer.on_table = None
-				consumer.session = None
-				consumer.save()
+			consumer = Consumer.objects.filter(on_table=table)[0]
+			consumer.session.close_session()
 			table.status = False
 			table.save()
 		response = dict(status='success')
@@ -688,8 +685,6 @@ def view_ajax_request(request):
 	except:
 		log_print(view_ajax_request) 
 		return HttpResponseBadRequest('Bad request')
-		
-		
 	
 #抢到的红包界面
 @csrf_exempt
